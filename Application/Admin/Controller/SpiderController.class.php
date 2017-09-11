5cc70ae2b44e9c774db0c1e9fc2eb91814963def
daimin <vagasnail@gmail.com>
2017-09-11 18:04:54 +0800

    爬虫框架

--- Application/Admin/Controller/SpiderController.class.php
+++ Application/Admin/Controller/SpiderController.class.php
@@ -14,12 +14,72 @@ require(THINK_PATH . "/../Snoopy.class.php");
 
 class SpiderController extends Controller {
     public function say(){
-        $snoopy = new \Snoopy;
+        $snoopy = new \Snoopy();
 
-// $snoopy->fetchtext("http://www.php.net/");
-// print $snoopy->results;
+        $sourceURL = "http://www.ci123.com/";
+        $snoopy->fetchlinks($sourceURL);
 
-        $snoopy->fetchlinks("http://www.ci123.com/");
-        var_dump($snoopy->results);
+        $as = $snoopy->results;
+        $re = "/\d+\.html$/";
+
+        //过滤获取指定的文件地址请求
+        foreach ($as as $tmp) {
+            if (preg_match($re, $tmp)) {
+                $this->getImgURL($tmp);
+            }
+        }
+    }
+
+    function getImgURL($siteName) {
+        $snoopy = new \Snoopy();
+        $snoopy->fetch($siteName);
+
+        $fileContent = $snoopy->results;
+
+        //匹配图片的正则表达式
+        $reTag = "/<img[^s]+src=\"(http:\/\/[^\"]+).(jpg|png|gif|jpeg)\"[^\/]*\/>/i";
+
+        if (preg_match($reTag, $fileContent)) {
+            $ret = preg_match_all($reTag, $fileContent, $matchResult);
+
+            for ($i = 0, $len = count($matchResult[1]); $i < $len; ++$i) {
+                $this->saveImgURL($matchResult[1][$i], $matchResult[2][$i]);
+            }
+        }
+    }
+
+    function saveImgURL($name, $suffix) {
+
+        $url = $name.".".$suffix;
+
+        echo "请求的图片地址：".$url."<br/>";
+
+        $imgSavePath = "/webser/www/yygcms/Uploads/";
+        $imgId = preg_replace("/^.+\/([0-9\-x]+)$/", "\\1", $name);
+
+        if ($suffix == "gif") {
+            $imgSavePath .= "emotion";
+        } else {
+            $imgSavePath .= "topic";
+        }
+
+        if (!file_exists($imgSavePath)){
+            mkdir ($imgSavePath, 0777, true);
+        }
+
+        $imgSavePath .= ("/".$imgId.".".$suffix);
+
+        if (is_file($imgSavePath)) {
+            unlink($imgSavePath);
+            echo "<p style='color:#f00;'>文件".$imgSavePath."已存在，将被删除</p>";
+        }
+
+        $imgFile = file_get_contents($url);
+        $flag = file_put_contents($imgSavePath, $imgFile);
+
+        if ($flag) {
+            echo "<p>文件".$imgSavePath."保存成功</p>";
+        }
+
     }
 }
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'Snoopy.class.php';
-$snoopy = new Snoopy;
-    
-// $snoopy->fetchtext("http://www.php.net/");
-// print $snoopy->results;
-
-    $snoopy->fetchlinks("http://www.ci123.com/");
-    var_dump($snoopy->results);
