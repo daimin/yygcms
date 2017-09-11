9911ddeed538e3919a560ffd84546cee8066ebce
daimin <vagasnail@gmail.com>
2017-09-11 17:51:03 +0800

    提交命令行模式

---
+++ Application/Admin/Controller/SpiderController.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daim01
+ * Date: 2017/9/11
+ * Time: 17:42
+ */
+
+namespace Admin\Controller;
+
+use \Think\Controller;
+
+class SpiderController extends Controller {
+    public function say(){
+        print_r('say some');
+    }
+}
--- cli.php
+++ cli.php
@@ -1,20 +1,17 @@
 <?php
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
-//普通模式，解决官方分组不支持cli的问题
-$depr = '/';
-$path   = isset($_SERVER['argv'][1])?$_SERVER['argv'][1]:'';
-if(!empty($path)) {
-    $params = explode($depr,trim($path,$depr));
-}
-!empty($params)?$_GET['g']=array_shift($params):"";
-!empty($params)?$_GET['m']=array_shift($params):"";
-!empty($params)?$_GET['a']=array_shift($params):"";
-if(count($params)>1) {
-// 解析剩余参数 并采用GET方式获取
-    preg_replace('@(\w+),([^,\/]+)@e', '$_GET[\'\\1\']="\\2";', implode(',',$params));
-}
- 
-//define('APP_MODE','cli');
-define('APP_DEBUG',True);
-define( 'APP_PATH', dirname(__FILE__).'/Application/' );
-require dirname(__FILE__).'/Core/ThinkPHP.php';
+// 检测PHP环境
+if (version_compare(PHP_VERSION, '5.3.0', '<')) die('require PHP > 5.3.0 !');
+//
+//// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+//define('APP_DEBUG', True);
+//
+define('MODE_NAME', 'cli');  // 采用CLI运行模式运行
+//
+//// 定义应用目录
+define('APP_PATH', 'Application/');
+//
+//// 绑定ap模块到当前入口文件
+////define('BIND_MODULE', 'V1');
+//
+//// 引入ThinkPHP入口文件
+require 'Core/ThinkPHP.php';
